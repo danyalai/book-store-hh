b04827fd47c54ed70609cb0de4dd496b10a36602
aryan <aryantahami@gmail.com>
2022-06-15 17:59:00 +0430

    add test

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,11 +6,10 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:  # برای اینکه اطلاعات اضافه به فرممون بدیم از متا استفاده میکنیم
         model = CustomUser
-        fields = ('username', 'age', 'email',)
+        fields = ('username', 'age', 'email', 'first_name', 'last_name', )
 
 
 class CustomUserChangeForm(UserChangeForm):  # 127-6
     class Meta:
         model = CustomUser
-        fields = ('username', 'age', 'email',)
-
+        fields = ('username', 'age', 'email', 'first_name', 'last_name', )
